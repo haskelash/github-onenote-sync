3a0f8fbc1fe08eadd9df95e63392061d4decedf0
Haskel Ash <haskelash@gmail.com>
2017-07-17 23:09:29 -0700

    add constants file

---
+++ Sources/App/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  GitHubOneNoteSync
+//
+//  Created by Haskel Ash on 7/16/17.
+//
+//
+
+import Foundation
+
+let oneNoteEndpoint = "https://www.onenote.com/api/v1.0/me/notes/notebooks"
+let tokenEndpoint = "https://login.live.com/oauth20_token.srf"
+let clientId = "5298f1b7-e039-4876-9f29-c6839239b008"
+let clientSecret = ""
+let redirectUri = "https://github-onenote-sync.herokuapp.com/info"
+
+let token = ""
+let refreshToken = ""
+
+let session = URLSession(configuration: .default)

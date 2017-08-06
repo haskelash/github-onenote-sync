14fdecc95fb706377b1dfa5ae011aab8526c3774
Haskel Ash <haskelash@gmail.com>
2017-08-06 15:43:57 -0700

    get rid of client secret and refresh token

--- Sources/Run/Constants.swift
+++ Sources/Run/Constants.swift
@@ -11,8 +11,6 @@ import Foundation
 let oneNoteEndpoint = "https://www.onenote.com/api/v1.0/me/notes/notebooks"
 let tokenEndpoint = "https://login.live.com/oauth20_token.srf"
 let clientId = "5298f1b7-e039-4876-9f29-c6839239b008"
-let clientSecret = ""
 let redirectUri = "https://github-onenote-sync.herokuapp.com/info"
-let refreshToken = ""
 
 let session = URLSession(configuration: .default)
